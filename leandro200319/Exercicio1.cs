958046185cc50ce7ea6bc7518ac8ba22269cabaf
Leandro <leandrorochaadm@gmail.com>
2020-03-22 22:56:46 -0400

    exercicio 1

---
+++ leandro200319/Exercicio1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace leandro200319
+{
+    class Exercicio1
+    {
+        public static void Exe()
+        {
+            Console.WriteLine("Faça um algoritmo para ler três valores reais e informar \n" +
+                "se estes podem ou não formar os lados de um triângulo e qual tipo de triângulo \n" +
+                "seria: equilátero, isósceles ou escaleno.");
+            Console.WriteLine();
+
+            Console.Write("Digite o tamanho do 1º lado: ");
+            Double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o tamanho do 2º lado: ");
+            Double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o tamanho do 3º lado: ");
+            Double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (a <= 0 | b <= 0 | c <= 0)
+            {
+                Console.WriteLine("Para formar um triângulo  precisa ter os lados maiores do que zero.");
+            }
+            else if (((a == b)&(a!=c)) | ((a == c)&(a!=b)) | ((b == c)&(b!=a)) )
+            {
+                Console.WriteLine("Esse é um triângulo Isósceles");
+            }
+            else if ((a == b) & (a == c) & (b == c))
+            {
+                Console.WriteLine("Esse é um triângulo Equilátero");
+            }
+            else
+            {
+                Console.WriteLine("Esse é um triângulo Escaleno");
+            }
+            
+
+            /*
+            Console.WriteLine(a.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(b.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(c.ToString("F2", CultureInfo.InvariantCulture));
+            */
+            Console.WriteLine();
+        }
+    }
+}
---
+++ leandro200319/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace leandro200319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Exercicio1 exercicio1 = new Exercicio1();
+
+            Console.WriteLine("Aluno: Leandro Rocha - IFRO - ADS - Algoritmo 1º periodo");
+            Console.WriteLine();
+
+            byte exercicio = byte.MaxValue;
+            while (exercicio != 0)
+            {
+                Console.WriteLine("Digite o número do exercício de 1 a 5 que deseja executar \nou digite número 0 para sair e depois presione tecla Enter");
+                exercicio = byte.Parse(Console.ReadLine());
+                Console.WriteLine();
+                
+                switch (exercicio)
+                {
+                    case 1:
+                        Exercicio1.Exe();
+                        break;
+                    case 2:
+                        Console.WriteLine("escolheu o exercício 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("escolheu o exercício 3");
+                        break;
+                    default: Console.WriteLine(">>> Número do exercício digitado inválido <<< \n");
+                        break;
+                }
+            }
+        }
+    }
+}
