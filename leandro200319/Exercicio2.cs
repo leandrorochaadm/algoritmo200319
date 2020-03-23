d47ff1147d888f92bdab72bc268de4911430ff46
Leandro <leandrorochaadm@gmail.com>
2020-03-22 23:11:44 -0400

    Exercicio 2

---
+++ leandro200319/Exercicio2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace leandro200319
+{
+    class Exercicio2
+    {
+        public static void Exe()
+        {
+            Console.WriteLine("Elabore um algoritmo que indique se um número digitado \n" +
+                "está compreendido entre 20 e 90, ou não.");
+
+            Console.Write("Digite um numero: ");
+            Double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if ((a>=20)&(a<=90))
+            {
+                Console.WriteLine("Este número digitado está compreendido entre 20 e 90");
+            }
+            else {
+                Console.WriteLine("Esse um número digitado NÃO está compreendido entre 20 e 90");
+            }
+            Console.WriteLine();
+        }
+    }
+}
